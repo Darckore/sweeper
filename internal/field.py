a37952b602645aac96b8c9244474186ca4379a2e
darckore <darckore@gmail.com>
2023-08-07 15:19:12 +0300

    cell neighbours

--- internal/field.py
+++ internal/field.py
@@ -8,7 +8,7 @@ import pygame
 # A cell on a board
 #
 class cell :
-  def __init__(self, centre : tuple[int, int], points : list) :
+  def __init__(self, centre : tuple[float, float], points : list) :
     self.points = points
     self.centre = centre
     self.neighbours = []
@@ -23,11 +23,15 @@ class rect_field :
   def __init__(self, cols : int, rows : int) :
     self.cols = cols
     self.rows = rows
+    self.__maxIdx = cols * rows
     self.dimensions = (self.cols * rect_field.cellSide, self.rows * rect_field.cellSide)
 
   # interface
 
-  def make_cells(self) -> list :
+  #
+  # Inits the required number of cells
+  #
+  def make_cells(self) -> list[cell] :
     cells = []
     for row in range (0, self.rows) :
       top = row * self.cellSide
@@ -35,7 +39,7 @@ class rect_field :
       for col in range (0, self.cols) :
         left = col * self.cellSide
         shiftedCol = left + self.cellSide
-        cells.append(cell((left + shiftedCol / 2, top + shiftedRow / 2),
+        cells.append(cell((left + self.cellSide / 2, top + self.cellSide / 2),
           [
             (left, top),
             (shiftedCol, top),
@@ -43,14 +47,41 @@ class rect_field :
             (shiftedCol, shiftedRow)
           ]))
 
-    return cells
+    return self.__init_neighbours(cells)
 
+  #
+  # Draws the rectangular board
+  #
   def draw(self, canvas : pygame.Surface, cells : list[cell],
                  boardColour : tuple[int, int, int], lineColour: tuple[int, int, int]) :
     for cell in cells :
       rect = pygame.Rect(cell.points[0][0], cell.points[0][1], self.cellSide, self.cellSide)
       pygame.draw.rect(canvas, boardColour, rect)
       pygame.draw.rect(canvas, lineColour, rect, 2)
+
+  # implementation
+
+  def __cell_at(self, cells : list[cell], col : int, row : int) -> cell :
+    if row < 0 or col < 0 or row >= self.rows or col >= self.cols :
+      return None
+    idx = row * self.cols + col
+    if idx < 0 or idx >= self.__maxIdx :
+      return None
+    return cells[idx]
+
+  def __init_neighbours(self, cells : list[cell]) -> list[cell]:
+    for cell in cells :
+      col = int(cell.points[0][0] / self.cellSide)
+      row = int(cell.points[0][1] / self.cellSide)
+      for y in range (-1, 2) :
+        for x in range (-1, 2) :
+          if x == 0 and y == 0 :
+            continue
+          neighbour = self.__cell_at(cells, col + x, row + y)
+          if neighbour != None :
+            cell.neighbours.append(neighbour)
+
+    return cells
 
 
 #
@@ -71,7 +102,7 @@ class board :
   #
   def make_rect(self, cols : int, rows : int) -> tuple[int, int] :
     self.minefield = rect_field(cols, rows)
-    self.cells = self.minefield.make_cells()
+    self.__init_cells()
     return self.minefield.dimensions
 
   #
@@ -83,3 +114,8 @@ class board :
       return
     
     self.minefield.draw(canvas, self.cells, self.boardColour, self.lineColour)
+
+  # implementation
+
+  def __init_cells(self) :
+    self.cells = self.minefield.make_cells()
